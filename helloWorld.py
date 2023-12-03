357132f80e7e43bcdecc54d8f4dbc2823d30dda2
johnkhumalo <johnkhumalo022@gmail.com>
2023-12-03 12:51:14 +0200

    Handle_empty_input

--- helloWorld.py
+++ helloWorld.py
@@ -1,3 +1,6 @@
-name=input("What is your name?")
-print(name)
+# Handle empty input
+while not name.strip():  # Check if the input is empty or whitespace
+    print("Please enter a valid name.")
+    name = input("What is your name? ")
+
 
